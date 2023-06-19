47f70366b072caa8859bf3f2771f9e43273ca8a8
x3m4llk0 <86302720+x3m4llk0@users.noreply.github.com>
2023-06-19 22:41:02 +0300

    Update settings.py
    /static/

--- website/website/settings.py
+++ website/website/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "website/static/",
 ]
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
