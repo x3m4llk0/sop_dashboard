e056002d0b8c1db80f3eef837cafd5cfb76e2489
YOUR_NAME <YOUR_EMAIL>
2023-09-12 00:05:21 +0300

    customize login form

--- website/db_api/forms.py
+++ website/db_api/forms.py
@@ -1,7 +1,16 @@
 from django.contrib.auth.forms import AuthenticationForm
 from .models import User
+from django import forms
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control py-4',
+        'placeholder': 'Введите пароль'
+    }))
     class Meta:
         model = User
         fields = ('username', 'password')    #может быть и [,]
